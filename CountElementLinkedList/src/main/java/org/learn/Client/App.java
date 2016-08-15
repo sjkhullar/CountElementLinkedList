cc5cf13ffc686ce822c1f0c871ed0a0547adf42e
makeinjava <makeinjava@gmail.com>
2016-08-15 23:21:41 +0530

    Updated code

--- CountElementLinkedList/src/main/java/org/learn/Client/App.java
+++ CountElementLinkedList/src/main/java/org/learn/Client/App.java
@@ -3,28 +3,26 @@ package org.learn.Client;
 import org.learn.List.Node;
 import org.learn.Question.CountElement;
 
-public class App 
-{
-	public static void main( String[] args )
-    {
-		int[] data = {10, 20, 10, 20, 50, 20, 50};
+public class App {
+	public static void main(String[] args) {
+		int[] data = { 10, 20, 10, 20, 50, 20, 50 };
 		Node head = new Node(data[0]);
-		for(int count = 1; count < data.length; count++)
-			CountElement.insert(head,data[count]);
-		
+		for (int count = 1; count < data.length; count++)
+			CountElement.insert(head, data[count]);
+
 		System.out.printf("Linked list is : ");
 		CountElement.print(head);
-		
-		int search = 10;		
+
+		int search = 10;
 		int count = CountElement.countElement(head, search);
-		System.out.printf("Element %d exists %d times in linked list",search,count);
-		
-		search = 20;		
+		System.out.printf("Element %d exists %d times in linked list", search, count);
+
+		search = 20;
 		count = CountElement.countElement(head, search);
-		System.out.printf("\nElement %d exists %d times in linked list",search,count);
-		
-		search = 50;		
+		System.out.printf("\nElement %d exists %d times in linked list", search, count);
+
+		search = 50;
 		count = CountElement.countElement(head, search);
-		System.out.printf("\nElement %d exists %d times in linked list",search,count);
-    }
+		System.out.printf("\nElement %d exists %d times in linked list", search, count);
+	}
 }
